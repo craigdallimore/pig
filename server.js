1cdaafbc09f67f52d1fe644e50ac3251acd7971d
craigdallimore <decoy9697@gmail.com>
2013-09-22 09:32:38 +0000

    Initial commit, nothing to see here

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express'),
     App = express(),
     server = require('http').createServer(App);
 
+
 // Connect configuration
 App.configure(function() {
     App.use('./static', express.static(__dirname + './static'));
